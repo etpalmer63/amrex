1a51b1017f356bf1dcbcb16f563c5db928a31cf4
Erik Palmer <epalmer@lbl.gov>
2022-05-20 16:16:22 -0400

    add find_package back

--- Tools/CMake/AMReXSYCL.cmake
+++ Tools/CMake/AMReXSYCL.cmake
@@ -4,7 +4,7 @@
 # For the time being, only dpc++ is supported
 #
 message("*** Using Custom AMReXSYCL file ****")
-#find_package(IntelDPCPP REQUIRED
+find_package(IntelDPCPP REQUIRED
 #    PATHS /soft/restricted/CNDA/sdk/2022.01.30.001/oneapi/compiler/pseudo-20220223/compiler/linux/IntelDPCPP )
 #message("*** SYCL_INCLUDE_DIR= ${SYCL_INCLUDE_DIR} ")
 #message("*** SYCL_LIBRARY_DIR= ${SYCL_LIBRARY_DIR} ")
