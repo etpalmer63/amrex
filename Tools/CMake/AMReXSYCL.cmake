74372a903b672f7f21ee76bba45da50de6ae06a7
Erik Palmer <ac.epalmer@jlselogin4.ftm.alcf.anl.gov>
2022-05-24 14:58:01 +0000

    path for IntelDPCPP package

--- Tools/CMake/AMReXSYCL.cmake
+++ Tools/CMake/AMReXSYCL.cmake
@@ -4,7 +4,8 @@
 # For the time being, only dpc++ is supported
 #
 message("*** Using Custom AMReXSYCL file ****")
-find_package(IntelDPCPP REQUIRED)
+find_package(IntelDPCPP REQUIRED
+	     PATH /soft/restricted/CNDA/sdk/2022.01.30.001/oneapi/compiler/pseudo-20220223/compiler/linux/IntelDPCPP)
 message("*** SYCL_INCLUDE_DIR= ${SYCL_INCLUDE_DIR} ")
 message("*** SYCL_LIBRARY_DIR= ${SYCL_LIBRARY_DIR} ")
 
