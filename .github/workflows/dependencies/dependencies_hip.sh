a5c1a364f0c6118b95ffb3808d68f49cbbd6c4a5
Erik Palmer <epalmer@lbl.gov>
2022-05-26 14:45:15 -0400

    add no-tty flag

--- .github/workflows/dependencies/dependencies_hip.sh
+++ .github/workflows/dependencies/dependencies_hip.sh
@@ -15,7 +15,7 @@ set -eu -o pipefail
 
 # Ref.: https://rocmdocs.amd.com/en/latest/Installation_Guide/Installation-Guide.html#ubuntu
 wget -q -O rocm.gpg.key https://repo.radeon.com/rocm/rocm.gpg.key
-sudo apt-key add rocm.gpg.key
+sudo apt-key add --no-tty rocm.gpg.key
 echo 'deb [arch=amd64] https://repo.radeon.com/rocm/apt/debian/ ubuntu main' \
   | sudo tee /etc/apt/sources.list.d/rocm.list
 echo 'export PATH=/opt/rocm/llvm/bin:/opt/rocm/bin:/opt/rocm/profiler/bin:/opt/rocm/opencl/bin:$PATH' \
